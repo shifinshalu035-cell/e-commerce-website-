a7a2130d0737c615e02d7c03c351ac3b775d3516
shifinshalu035-cell <shifinshalu035@gmail.com>
2026-06-24 08:09:15 +0530

    lsited some feture product card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,45 +17,25 @@ function Home() {
         console.log(error);
       });
   }, []);
-  console.log(products)
 
   return (
     <div>
       <HeroSection />
 
-
       <CategorySection />
+
       <h1 className="text-3xl font-bold text-center my-8">
         Our Featured Products
       </h1>
-<div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 p-4">
-  {products.map((product) => (
-    <div
-      key={product.id}
-      className="bg-white rounded-lg shadow-md overflow-hidden hover:scale-105 transition"
-    >
-      <img
-        src={
-          Array.isArray(product.image)
-            ? product.image[0]
-            : product.image
-        }
-        alt={product.title}
-        className="w-full h-24 object-cover"
-      />
 
-      <div className="p-2">
-        <h2 className="font-bold text-xs">
-          {product.title}
-        </h2>
-
-        <p className="text-green-600 font-bold text-sm mt-1">
-          ₹{product.price}
-        </p>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
       </div>
-    </div>
-  ))}
-</div>
     </div>
   );
 }
